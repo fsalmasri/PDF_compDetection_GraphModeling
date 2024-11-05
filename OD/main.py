7db3ae2f9357b2fb98453d387b80729eee7dfe8a
feras <fsalmasri@hotmail.com>
2024-11-05 10:39:35 +0100

    training

--- OD/main.py
+++ OD/main.py
@@ -5,76 +5,101 @@ from ultralytics import settings
 
 import os
 import torch
-import fitz
+# import fitz
 from PIL import Image
 
 
-ds_dir = 'LOGIC_157_YOLO_v2'
+# ds_dir = 'LOGIC_157_YOLO_v2'
 
 def run():
-    settings.update({'runs_dir': '/runs',
+    # model_name = 'yolov8n_custom1'
+    saving_model_name = 'yolov8n_custom7'
 
-                     'tensorboard': True,
-                     'visualize': True,
-                     'show_labels': False,
-                     'datasets_dir': rf'C:\Users\fsalm\Desktop\DISTILL\{ds_dir}'})
+    settings.update({
+        'runs_dir': '/runs',
+
+        'tensorboard': True,
+        'visualize': True,
+        'show_labels': False,
+        'visualize': True,
+    })
 
     # Load the model.
-    model = YOLO('yolov8n.pt', task='detect')
-    ARGS = {
-        'data': r'C:\Users\fsalm\Desktop\DISTILL\Distill\OD\ds.yaml',
-        'imgsz': 1280,
-        'epochs': 100,
-        'batch': 8,
-        'name': 'yolov8n_custom',
+    # model = YOLO('yolo11n.pt', task='detect')
+    model = YOLO('yolo11s.pt', task='detect')
 
-        'scale': 0.5,
-        'mosaic': 0,
-    }
-    # model = YOLO('runs/detect/yolov8n_custom2/weights/best.pt', task='detect')
 
+    results = model.train(
+        data='/home/feras/Desktop/DISTILL/Distill/OD/ds.yaml',
+        imgsz=1280,
+        epochs=500,
+        batch=8,
+        workers=8,
+        device=[0, 1],
+
+        pose=0,
+        kobj=0,
+        cls=1,
+
+        lr0=0.001,
+
+        dropout=0.4,
+        scale=0.9,
+        mosaic=0,
+        fliplr=False,
+        show_labels=False,
+        show_conf = True,
+
+        name=f'{saving_model_name}',
+        # resume=True,
+        save=True,
+
+        line_width=1,
+
+    )
     # Training.
-    model.train(**ARGS)
+    # model.train(**ARGS)
 
 
 def test(no):
     model = YOLO(f'runs/detect/yolov8n_custom{no}/weights/best.pt', task='detect')
-    test_dir = rf'C:\Users\fsalm\Desktop\DISTILL\{ds_dir}\test\images'
+    test_dir = f'../../Distill_logics_cropped/test/images/'
 
     for test_im in os.listdir(test_dir):
-        results = model.predict(os.path.join(test_dir, test_im), save=False, line_width=1, conf=.4)
+        results = model.predict(os.path.join(test_dir, test_im), save=True, line_width=1, conf=.4)
         result = results[0]
         boxes = result.boxes.xyxy.cpu().numpy()
         confidences = results[0].boxes.conf.cpu().numpy()
         classes = result.boxes.cls.cpu().numpy()
         names = result.names
 
+        #
+        # print(names)
+        # exit()
 
-        print(names)
-
-        predicted_LC_bbxs = boxes[classes == 0]
-        predicted_LC_conf = confidences[classes == 0]
-
-        predicted_LCCON_bbxs = boxes[classes == 1]
-        predicted_LCCON_conf = confidences[classes == 1]
-
-        predicted_LCInp_bbxs = boxes[classes == 2]
-        predicted_LCInp_conf = confidences[classes == 2]
-
-        predicted_txt_bbxs = boxes[classes == 3]
-        predicted_txt_conf = confidences[classes == 3]
-
-        print(len(classes), len(predicted_LC_bbxs), len(predicted_LCCON_bbxs))
-
-        np.save('LC_bbx.npy', predicted_LC_bbxs)
-        np.save('LCCON_bbx.npy', predicted_LCCON_bbxs)
-        np.save('LCInp_bbx.npy', predicted_LCInp_bbxs)
-        np.save('LCTXT_bbx.npy', predicted_txt_bbxs)
-        exit()
+        # predicted_LC_bbxs = boxes[classes == 0]
+        # predicted_LC_conf = confidences[classes == 0]
+        #
+        # predicted_LCCON_bbxs = boxes[classes == 1]
+        # predicted_LCCON_conf = confidences[classes == 1]
+        #
+        # predicted_LCInp_bbxs = boxes[classes == 2]
+        # predicted_LCInp_conf = confidences[classes == 2]
+        #
+        # predicted_txt_bbxs = boxes[classes == 3]
+        # predicted_txt_conf = confidences[classes == 3]
+        #
+        # print(len(classes), len(predicted_LC_bbxs), len(predicted_LCCON_bbxs))
+        #
+        # np.save('LC_bbx.npy', predicted_LC_bbxs)
+        # np.save('LCCON_bbx.npy', predicted_LCCON_bbxs)
+        # np.save('LCInp_bbx.npy', predicted_LCInp_bbxs)
+        # np.save('LCTXT_bbx.npy', predicted_txt_bbxs)
+        # exit()
         #     result.save(show_labels=False)
 
 
 
 if __name__ == '__main__':
-    # run()
-    test(7)
+    run()
+    # test(6)
